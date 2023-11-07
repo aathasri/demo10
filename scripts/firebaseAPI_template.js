3694f40c5785fa2a343e2d87a0381502691f8724
aathasri <asriharan@my.bcit.ca>
2023-11-07 15:37:15 -0800

    Did some javascripting stuff

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,7 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-
+    apiKey: "AIzaSyB3MhMUhQhXPbyjqUlG3rn-D-Bc2eO9en4",
+    authDomain: "demo10-9c616.firebaseapp.com",
+    projectId: "demo10-9c616",
+    storageBucket: "demo10-9c616.appspot.com",
+    messagingSenderId: "312705229291",
+    appId: "1:312705229291:web:76dbff2103618ed83039ae"
 };
 
 //--------------------------------------------
